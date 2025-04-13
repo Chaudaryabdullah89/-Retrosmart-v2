a446c17b7a0ca6255cc7e852161d19b633370cbf
Chaudaryabdullah <chaudaryabdullah899@gmail.com>
2025-04-13 12:06:23 +0500

    23

--- Pages/EcoInstallation.jsx
+++ Pages/EcoInstallation.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Eligibilityform from '../components/Eligibilityform'
 import HomeBenfits from '../components/HomeBenfits'
-import Team from '../components/team'
+import Team from '../components/Team'
 
 const EcoInstallation = () => {
   return (
